51d6e4b5f17afbc7e769cdc63f1a2a54ffc692de
World Thirteen <mihailzachepilo@gmail.com>
2017-02-17 17:40:59 +0200

    it type

--- src/state/id.js
+++ src/state/id.js
@@ -1,4 +1,10 @@
+import UUID from 'uuid';
 import TypedNode from '../node';
 
 export default class ID extends TypedNode {
+  constructor(value) {
+    super();
+
+    this.value = value || UUID();
+  }
 }
---
+++ test/state/id.js
@@ -0,0 +1,16 @@
+import { expect, assert } from 'chai';
+import { is, Schema, Type, State } from '../../src';
+
+
+describe('ID', () => {
+  it('Init without value', () => {
+    const id = new State.ID();
+    assert.equal(id.get().length, 36);
+  });
+
+  it('Iinit with value', () => {
+    const value = 'idididid';
+    const id = new State.ID(value);
+    assert.equal(value, value);
+  });
+});
