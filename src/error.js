dd0914579ae9d2b98cfea75987e3c459dd981d0f
worldthirteen <mihailzachepilo@gmail.com>
2017-09-25 18:37:03 +0300

    Add error message formatter

--- src/error.js
+++ src/error.js
@@ -1,14 +1,59 @@
 import { sym } from './utils';
 import is from './is';
 
+function tab(depth) {
+  return '  '.repeat(depth);
+}
+
 export class DefaultError {
-  constructor() {
+  constructor(message) {
     this[sym('error')] = true;
     this.count = 1;
+    this.message = message;
+  }
+
+  formatMessage() {
+    return this.message;
   }
 
   static formatError(err) {
-    return err;
+    function formatErrorPart(obj, depth, padBrackets) {
+      if (depth === 0 && typeof obj === 'object') {
+        return `\n${obj.name} ${formatErrorPart(obj, 1)}`;
+      }
+
+      if (obj instanceof DefaultError) {
+        return `'${obj.formatMessage().slice(0, -1)}'\n`;
+      }
+
+      if (typeof obj === 'object' && obj.list) {
+        return `[\n${
+          obj.list.map(o => formatErrorPart(o, depth + 1, true)).join('')
+        }${tab(depth - 1)}]\n`;
+      }
+
+      if (typeof obj === 'object' && obj.map) {
+        const children = obj.map;
+        const padLeft = tab(padBrackets ? depth - 1 : 0);
+
+        return `${padLeft}{\n${
+          Object.keys(children).filter((key) => {
+            if (key === 'name' && typeof children[key] === 'string') {
+              return false;
+            }
+            if (key === 'count') {
+              return false;
+            }
+            return true;
+          })
+            .map(key =>
+              `${tab(depth)}${key}: ${formatErrorPart(children[key], depth + 1)}`).join('')
+        }${tab(depth - 1)}}\n`;
+      }
+
+      return '';
+    }
+    return formatErrorPart(err, 0);
   }
 }
 
