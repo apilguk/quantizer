072282483a13b59b7bbeb75bcea332e3ce72b222
apilguk@gmail.com <apilguk@gmail.com>
2018-10-17 04:17:46 +0300

    added typing

--- index.d.ts
+++ index.d.ts
@@ -4,39 +4,44 @@ export class TypedNode<T> {
 }
 
 export module State {
-  export class Map<T> extends TypedNode<T> {
-    public get(...args: (keyof T)[]): any;
-    public set(a: T | keyof T, b?: any): void;
-    public merge(a: T | string, b?: any): void;
-    public setAttribute(a: string, b?: any): void;
-    public getAttribute(a: string): any;
-    public map(handler: (value: any, key?: string) => any): any;
-    public find(key: string): any;
+  export class Map<T> {
+    public get(): T;
+    public get<K extends keyof T>(key: K): T[K];
+    public get<K extends keyof T>(...args: K[]): Pick<T, K>;
+    public set(a: T): void;
+    public merge<K extends keyof T>(a: Partial<T>): void;
+    public merge<K extends keyof T>(a: K, b: T[K]): void;
+    public setAttribute<K extends keyof T>(a: K, b: T[K]): void;
+    public getAttribute<K extends keyof T>(a: K): T[K];
+    public map(handler: (value: any, key?: string) => T): T;
+    public find<K extends keyof T>(key: K): any;
     public clone(): this;
     public toJSON(): string;
     public fromJSON(str: string): this;
   }
 
-  export class List<T> extends TypedNode<T> {
-    public set(source: T | any): any;
-    public get(...args: (keyof T)[]): any;
+  export class List<T> {
+    public set(source: Partial<T>): void;
+    public get(): T[];
+    public get<K extends keyof T>(key: K): T[K][];
+    public get<K extends keyof T>(...args: K[]): Pick<T, K>[];
     public clear(): void;
-    public concat(source: any[]): void;
+    public concat(source: T[]): void;
     public push(element: T): void;
     public map(handler: (value: T, index?: string) => any): T[];
-    public at(index: number): T;
-    public where(key: string, value: any): T;
+    public at(index: number): T | undefined;
+    public where<K extends keyof T>(key: K, value: T[K]): T | undefined;
     public filter(index: number): T[];
-    public sortBy(key: string): this;
-    public remove(toRemove: any): void;
+    public sortBy<K extends keyof T>(key: K): this;
+    public remove(toRemove: this): void;
   }
 
-  export class Boolean extends TypedNode<boolean> { }
-  export class String extends TypedNode<string> { }
-  export class Number extends TypedNode<number> { }
-  export class ObjectID extends TypedNode<string> { }
-  export class UUID extends TypedNode<string> { }
-  export class Any extends TypedNode<any> { }
+  export class Boolean extends TypedNode<boolean> {}
+  export class String extends TypedNode<string> {}
+  export class Number extends TypedNode<number> {}
+  export class ObjectID extends TypedNode<string> {}
+  export class UUID extends TypedNode<string> {}
+  export class Any extends TypedNode<any> {}
 }
 
 export class Schema {
@@ -53,3 +58,5 @@ export class Type {
   static UUID: Type;
   static ObjectID: Type;
 }
+
+
